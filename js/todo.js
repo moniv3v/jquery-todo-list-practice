ea8f5ebaa1dbbd47332176f933c5f136ba6877ac
Monique <moniquechan1220@gmail.com>
2018-11-15 16:19:58 +0800

    Updated

--- js/todo.js
+++ js/todo.js
@@ -21,5 +21,77 @@ $(document)
             return uuid;
         }
 
+        function addToDo() {
+            var input = $('#input').val();
+            $('ol').append(`<li id=` + generateUUID() + ` class=""><input name="done-todo" type="checkbox" class="done-todo"><span>` + input + `</span></li>`);
+            $('#input').val("");
+        }
+
         // code to be implemented
+        $("#button").click(addToDo);
+
+        $('input').focus(function () {
+            $(this).val('');
+        });
+
+        $("#input").keyup(function (e) {
+            if (e.keyCode == 13) {
+                addToDo();
+            }
+        });
+
+        $(document).on("change", ":checkbox", function () {
+            if ($(this).is(':checked')) {
+                $(this).parent().addClass('checked');
+            } else {
+                $(this).parent().removeClass('checked');
+            }
+        });
+
+        //all
+        $(document).on("click","a[data-filter='all']",function() {
+            $("#filters li a").removeClass('selected');
+            $(this).addClass('selected');
+            $(".done-todo").each(function () {
+                $(this).parent().show();
+            })
+        })
+
+         //"active"
+        $(document).on("click","a[data-filter='active']",function() {
+            $("#filters li a").removeClass('selected');
+            $(this).addClass('selected');
+            $("input[name='done-todo']").each(function (){
+                if($(this).is(":checked")){
+                  $(this).parent().hide();
+                }else{
+                  $(this).parent().show();
+                }
+            })
+        })
+
+         //"complete"
+        $(document).on("click","a[data-filter='complete']",function() {
+            $("#filters li a").removeClass('selected');
+                    $(this).addClass('selected');
+            $("input[name='done-todo']").each(function (){
+                if($(this).is(":checked")){
+                    $(this).parent().show();
+                }else{
+                  $(this).parent().hide();
+                }
+            })
+        })
+
+        $(document).on("dblclick","li",function(){
+            $(this)
+            .children('span').attr('contentEditable',true)
+            .keypress(function(e){
+                if(e.keyCode==13){
+                    e.target.blur();
+                    $(this)
+                    .children('span').attr('contentEditable',false);
+                }
+            })
+        })
     });
